377ad3de56c7cf1d44b44d5da0bb1849d27e6d28
laim2003 <luk.faber@gmx.de>
2019-08-20 13:42:45 +0200

    initial commit

---
+++ core/src/kotlin/extensioneer/Base.kt
@@ -0,0 +1,29 @@
+package extensioneer
+
+fun <T> T?.notNull(block: T.() -> Unit): T? {
+    if (this != null) {
+        block()
+    }
+    return this
+}
+
+fun <T> T?.isNull(block: T?.() -> Unit): T? {
+    if (this == null) {
+        block()
+    }
+    return this
+}
+
+fun <T, R> T?.notNullWithResult(block: T.() -> R?): R? {
+    if (this != null) {
+        return block()
+    }
+    return this
+}
+
+fun <T, R> T?.isNullWithResult(block: T?.() -> R?): R? {
+    if (this == null) {
+        return block()
+    }
+    return null
+}
