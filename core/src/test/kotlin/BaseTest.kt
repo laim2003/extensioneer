c284bb3232eba9ddf6a3ae6cdf2037f1c48464ea
Lukas Faber <lukas.faber@raysono.com>
2019-08-20 14:35:17 +0200

    initial commit

--- core/src/test/kotlin/BaseTest.kt
+++ core/src/test/kotlin/BaseTest.kt
@@ -1,58 +1,58 @@
-import extensioneer.isNull
-import extensioneer.isNullWithResult
-import extensioneer.notNull
-import extensioneer.notNullWithResult
-import org.junit.Assert.assertEquals
-import org.junit.Test
-
-class BaseTest {
-    @Test
-    fun testIsNull() {
-        val nullVar: Any? = null
-        var isNull = false
-        nullVar.isNull {
-            isNull = true
-        }
-        assertEquals(true, isNull)
-    }
-
-    @Test
-    fun testNotNull() {
-        val nullableVar: String? = "testString"
-        var isNull = true
-        nullableVar.notNull {
-            isNull = false
-        }
-        assertEquals(false, isNull)
-    }
-
-    @Test
-    fun testNotNullWithResult() {
-        var expectedResult: String? = null
-        var actualResult: String? = expectedResult.notNullWithResult {
-            return@notNullWithResult this
-        }
-        assertEquals(null, actualResult)
-
-        expectedResult = "testResult"
-        actualResult = expectedResult.notNullWithResult {
-            return@notNullWithResult this
-        }
-        assertEquals(expectedResult, expectedResult)
-    }
-
-    @Test
-    fun testIsNullWithResult() {
-        var expectedResult: String? = null
-        var actualResult: String? = expectedResult.isNullWithResult {
-            return@isNullWithResult this
-        }
-        assertEquals(actualResult, null)
-
-        expectedResult = "testResult"
-        actualResult = expectedResult.isNullWithResult {
-            return@isNullWithResult this
-        }
-        assertEquals(actualResult, null)
-    }
-}
+//import extensioneer.isNull
+//import extensioneer.isNullWithResult
+//import extensioneer.notNull
+//import extensioneer.notNullWithResult
+//import org.junit.Assert.assertEquals
+//import org.junit.Test
+//
+//class BaseTest {
+//    @Test
+//    fun testIsNull() {
+//        val nullVar: Any? = null
+//        var isNull = false
+//        nullVar.isNull {
+//            isNull = true
+//        }
+//        assertEquals(true, isNull)
+//    }
+//
+//    @Test
+//    fun testNotNull() {
+//        val nullableVar: String? = "testString"
+//        var isNull = true
+//        nullableVar.notNull {
+//            isNull = false
+//        }
+//        assertEquals(false, isNull)
+//    }
+//
+//    @Test
+//    fun testNotNullWithResult() {
+//        var expectedResult: String? = null
+//        var actualResult: String? = expectedResult.notNullWithResult {
+//            return@notNullWithResult this
+//        }
+//        assertEquals(null, actualResult)
+//
+//        expectedResult = "testResult"
+//        actualResult = expectedResult.notNullWithResult {
+//            return@notNullWithResult this
+//        }
+//        assertEquals(expectedResult, expectedResult)
+//    }
+//
+//    @Test
+//    fun testIsNullWithResult() {
+//        var expectedResult: String? = null
+//        var actualResult: String? = expectedResult.isNullWithResult {
+//            return@isNullWithResult this
+//        }
+//        assertEquals(actualResult, null)
+//
+//        expectedResult = "testResult"
+//        actualResult = expectedResult.isNullWithResult {
+//            return@isNullWithResult this
+//        }
+//        assertEquals(actualResult, null)
+//    }
+//}
---
+++ files/src/main/kotlin/extensioneer/files/InputStreamExt.kt
@@ -0,0 +1,16 @@
+package extensioneer.files
+
+
+import extensioneer.notNull
+import java.io.File
+import java.io.InputStream
+
+fun InputStream.toFile(dest: String): File {
+    val file = File(dest)
+    file.parentFile.notNull {
+        mkdirs()
+    }
+    file.createNewFile()
+    file.writeBytes(readBytes())
+    return file
+}
