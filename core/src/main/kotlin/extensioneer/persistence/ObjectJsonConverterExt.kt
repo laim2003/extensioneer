1a7a786caf6d12e70b27a1d9dbe0a3853de4f811
Lukas Faber <luk.faber@gmx.de>
2021-01-18 00:20:55 +0100

    - to json converted objects are now wrapped inside a JSON object that is named after the qualified class name od the source class that will be converted

--- core/src/main/kotlin/extensioneer/persistence/ObjectJsonConverterExt.kt
+++ core/src/main/kotlin/extensioneer/persistence/ObjectJsonConverterExt.kt
@@ -1,14 +1,11 @@
 package extensioneer.persistence
 
-import extensioneer.isNull
-import extensioneer.notNull
 import extensioneer.org.json.forEach
 import org.json.JSONArray
 import org.json.JSONObject
 import java.lang.RuntimeException
 import kotlin.reflect.full.isSubtypeOf
 import kotlin.reflect.full.memberProperties
-import kotlin.reflect.jvm.javaField
 import kotlin.reflect.jvm.javaType
 import kotlin.reflect.typeOf
 
@@ -29,6 +26,84 @@ class innererSubClass {
     var lslls = "sdjfkskldfj"
 }
 
+val testJSON = """
+{
+   "sub":{
+      "class":"extensioneer.persistence.SubClass",
+      "value":{
+         "sss":{
+            "class":"int",
+            "value":3
+         },
+         "name":{
+            "class":"java.lang.String",
+            "value":"dfsdd"
+         },
+         "innererSubClass":{
+            "class":"extensioneer.persistence.innererSubClass",
+            "value":{
+               "lslls":{
+                  "class":"java.lang.String",
+                  "value":"sdjfkskldfj"
+               }
+            }
+         },
+         "list":{
+            "class":"java.lang.Void",
+            "value":"null"
+         }
+      }
+   },
+   "list2":{
+      "class":"java.util.List<java.lang.String>",
+      "value":[
+         "",
+         ""
+      ]
+   },
+   "subss":{
+      "class":"extensioneer.persistence.SubClass",
+      "value":{
+         "sss":{
+            "class":"int",
+            "value":3
+         },
+         "name":{
+            "class":"java.lang.String",
+            "value":"dfsdd"
+         },
+         "innererSubClass":{
+            "class":"extensioneer.persistence.innererSubClass",
+            "value":{
+               "lslls":{
+                  "class":"java.lang.String",
+                  "value":"sdjfkskldfj"
+               }
+            }
+         },
+         "list":{
+            "class":"java.lang.Void",
+            "value":"null"
+         }
+      }
+   },
+   "list":{
+      "class":"java.lang.String[]",
+      "value":[
+         "one",
+         "tow",
+         "thre"
+      ]
+   },
+   "map":{
+      "class":"java.util.Map<java.lang.String, java.lang.Integer>",
+      "value":{
+         "o":2
+      }
+   }
+}
+""".trimIndent()
+
 @ExperimentalStdlibApi
 fun main() {
     val test = TestClass()
@@ -40,8 +115,13 @@ fun Any.convertToJsonObject(): JSONObject {
 }
 
 @ExperimentalStdlibApi
-private fun Any.writeClassToJson(): JSONObject {
-    val classJSON = JSONObject()
+fun Any.writeClassToJson() : JSONObject {
+    val classJson = JSONObject()
+    return classJson.put(this::class.qualifiedName, this.writeClassToJsonIterable())
+}
+
+@ExperimentalStdlibApi
+private fun Any.writeClassToJsonIterable(classJSON: JSONObject = JSONObject()): JSONObject {
     val objectProperties = this::class.memberProperties
 
     objectProperties.forEach {
@@ -64,10 +144,10 @@ private fun Any.writeClassToJson(): JSONObject {
                 classJSON.put(it.name, mapOf("class" to it.returnType.javaType.typeName, "value" to "null"))
             } else {
                 val value = it.getter.call(this)
-                classJSON.put(it.name, mapOf("class" to it.returnType.javaType.typeName, "value" to value!!.writeClassToJson()))
+                classJSON.put(it.name, mapOf("class" to it.returnType.javaType.typeName, "value" to value!!.writeClassToJsonIterable()))
             }
         } else {
-            classJSON.put(it.name, it.getter.call(this))
+            classJSON.put(it.name, mapOf("class" to it.returnType.javaType.typeName, "value" to it.getter.call(this)))
         }
     }
     return classJSON
@@ -80,9 +160,13 @@ inline fun <reified returnType> String.readJsonToClass(): returnType {
     classJson.forEach {
         val value = it.second
         if (value is JSONObject) {
+            val fieldInfo = value
+            val fieldClassInstance = Class.forName(fieldInfo.getString("class")).getDeclaredConstructor().newInstance()
 
+            println(fieldClassInstance)
+
+            returnClass.getField(it.first).set(null, fieldInfo.get("value"))
         }
-        returnClass.getField(it.first).set(this, it.second)
     }
     return returnClass.getDeclaredConstructor().newInstance()
 }
